4d37a2aa91144256960a507a2437ac95546148e6
ZachGraceman <zach.graceman@gmail.com>
2022-12-05 15:00:29 -0600

    Commiting lesson 2 and 3 assignments

---
+++ src/perscholas/module303/lesson2/StringsAssignment.java
@@ -0,0 +1,86 @@
+package perscholas.module303.lesson2;
+
+import java.io.*;
+import java.util.*;
+
+
+public class StringsAssignment {
+	
+	public static void main(String[] args) {
+		stringReverse();
+		stringIntroduction();
+		stringTokens();
+		stringComparisons();
+		substrings();	
+	}
+	
+	public static void stringReverse() {
+		Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        
+        String reverse = new StringBuilder(A).reverse().toString();
+        if (A.compareTo(reverse) == 0) {
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+	}
+	
+	public static void stringIntroduction() {
+		Scanner sc=new Scanner(System.in);
+		String A=sc.next();
+		String B=sc.next();
+    
+		System.out.println(A.length() + B.length()); // part 1
+		if (A.compareTo(B) > B.compareTo(A)) {System.out.println("Yes");} // part 2
+		else {System.out.println("No");} // part 2
+		System.out.println(A.substring(0, 1).toUpperCase() + A.substring(1) + " " + B.substring(0, 1).toUpperCase() + B.substring(1)); // part 3
+    }
+	
+	public static void stringTokens() {
+		Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        s = s.trim();
+        if (s.length()==0) {System.out.println(0); return;}
+        String[] tokens = s.split("[^a-zA-Z]+");
+        System.out.println(tokens.length);
+        for (int i=0; i < tokens.length; i++) {
+            System.out.println(tokens[i]);
+        }
+        
+        scan.close();
+	}
+	
+	public static void stringComparisons() {
+		Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+        
+		String smallest = s.substring(s.length()-k);
+        String largest = s.substring(0, k);
+        
+        for(int i = 0; i <= s.length() - k; i++){
+            String s2 = s.substring(i, k + i);
+            if (smallest.compareTo(s2) > 0){
+                smallest = s2;
+            }
+            if (largest.compareTo(s2) < 0){
+                largest = s2;
+            }
+        }
+        
+        System.out.println(smallest + "\n" + largest);
+	}
+	
+	public static void substrings() {
+		Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        System.out.println(S.substring(start, end));
+	}
+
+
+}
---
+++ src/perscholas/module303/lesson3/LoopsAssignment.java
@@ -0,0 +1,65 @@
+package perscholas.module303.lesson3;
+
+import java.util.Scanner;
+
+public class LoopsAssignment {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Question 1:"); multiplicationTable(); // Question 1 
+		System.out.println("\nQuestion 2:"); greatestCommonDivisor(); // Question 2 
+		System.out.println("\nQuestion 3:"); predictFutureTuition(); // Question 3
+
+	}
+	
+	public static Scanner sc=new Scanner(System.in);
+
+	// Question 1
+	public static void multiplicationTable() {
+		
+		/* Write a program that uses for loops to print a multiplication table.*/
+		
+		for (int i = 1; i <= 12; i++) {
+			System.out.print("\n" + i + " ");
+			for (int j = 2; j <= 12; j++) {
+				System.out.print(j*i + " ");
+			}
+		}
+	}
+	
+	// Question 2
+	public static void greatestCommonDivisor() {
+		
+		/* Write a program that prompts the user to enter two positive integers, and find their greatest common divisor (GCD).*/
+		
+		System.out.print("Enter positive integer: ");
+		int a = sc.nextInt();
+		System.out.print("Enter another positive integer: ");
+		int b = sc.nextInt();
+		int gcd = 1;
+		
+		for (int i = 1; i <= a && i <= b; i++) {
+			if (a%i == 0 && b%i == 0) {
+				gcd = i;
+			}
+		}
+		System.out.println("Greatest common divisor = " + gcd);
+		
+	}
+	
+	// Question 3
+	public static void predictFutureTuition() {
+		
+		/* Suppose the tuition for a university is $10,000 for the current year and increases by 7% every year
+		 * In how many years will the tuition be doubled?*/
+		int years = 0;
+		double tuition = 10000;
+		do {
+			tuition = tuition * 1.07;
+			years++;
+		}
+		while (tuition < 20000);
+		System.out.println("In " + years + " years tuition will have doubled.");
+	}
+
+}
